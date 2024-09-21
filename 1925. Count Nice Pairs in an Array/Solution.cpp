b0beb02029697ca4e3729cae7c516db8ab2a2cc1
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-09-21 20:12:49 +0530

    C++ Solution
            Submission Statistics:
            Question #: 1925
            Language: C++
            Runtime: 87 ms
            Runtime percentile: 73.31
            Memory: 60.696 MB
            Memory percentile: 73.75

--- 1925. Count Nice Pairs in an Array/Solution.cpp
+++ 1925. Count Nice Pairs in an Array/Solution.cpp
@@ -1,36 +1,29 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     long long int mod = 1e9 + 7;
-
-    // Function to reverse an integer
-    int rev(int n) {
+    int rev(int n){
         int r = 0;
-        while (n > 0) {
-            r = r * 10 + (n % 10);
+        while(n > 0){
+            r *= 10;
+            r += (n%10);
             n /= 10;
         }
         return r;
     }
 
     int countNicePairs(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        long long count = 0;
-
-        // Calculate the difference for each number and populate the frequency map
-        for (int i = 0; i < nums.size(); ++i) {
-            int diff = nums[i] - rev(nums[i]);
-            // Add current count of 'diff' to result as those many pairs can be made with this new occurrence
-            if (freq.find(diff) != freq.end()) {
-                count = (count + freq[diff]) % mod;
-            }
-            // Increment frequency of this diff
-            freq[diff]++;
+        int n = nums.size();
+        int count = 0;
+        unordered_map<int,int> mp;
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] - rev(nums[i]);
         }
-
+        for(int i=0; i<n; i++){
+            if(mp.find(nums[i]) != mp.end()){
+                count = (count + mp[nums[i]]) % mod;
+            }
+            mp[nums[i]]++;
+        }
         return count;
     }
 };
