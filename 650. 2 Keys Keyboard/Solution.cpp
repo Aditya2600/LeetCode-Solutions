7dd06d37cc8ae99227f2257fa0061ea2a055c7c3
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-04-13 10:26:55 +0530

    C++ Solution
          Submission Statistics:
          Question #: 650
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 7.748 MB
          Memory percentile: 93.63

--- 650. 2 Keys Keyboard/Solution.cpp
+++ 650. 2 Keys Keyboard/Solution.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+    // int gd(int n){
+    //     for(int i=n/2; i>=1; i--){
+    //         if(n%i == 0) return i;
+    //     }
+    //     return 1;
+    // }
+    bool isPrime(int n){
+        if(n == 1) return false;
+        for(int i=2; i<=sqrt(n); i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+
     int gd(int n){
-        for(int i=n/2; i>=1; i--){
-            if(n%i == 0) return i;
+        for(int i=2; i<=sqrt(n); i++){
+            if(n%i == 0) return n/i;
         }
         return 1;
     }
+
     int minSteps(int n) {
         int count = 0;
         while(n>1){
+            if(isPrime(n)){
+                count+=n;
+                break;
+            }
             int hf = gd(n);
             count += (n/hf);
             n = hf;
