9d608639816ed53fdab157f9b98c05c75993221e
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-20 23:21:18 +0530

    C++ Solution
          Submission Statistics:
          Question #: 279
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 8.112 MB
          Memory percentile: 99.41

--- 279. Perfect Squares/Solution.cpp
+++ 279. Perfect Squares/Solution.cpp
@@ -18,14 +18,32 @@ public:
     // }
 
     int numSquares(int n){
-        vector<int> dp = vector<int> (n+1, 0);
-        for(int i=1; i<=n; i++){
-            dp[i] = i;
-            for(int j=1; j*j<=i; j++){
-                int square = j*j;
-                dp[i] = min(dp[i], 1 + dp[i - square]);
+        int sqrtVal = (int) sqrt(n);
+        if(sqrtVal * sqrtVal == n) return 1;
+
+        while(n % 4 == 0){
+            n /= 4;
+        }
+        if(n % 8 == 7){
+            return 4;
+        }
+        for(int i=1; i*i <= n; i++){
+            int squareA = i*i;
+            int elementB = (int) sqrt(n - squareA);
+            if(elementB * elementB == n - squareA){
+                return 2;
             }
         }
-        return dp[n];
+        return 3;
+
+        // vector<int> dp = vector<int> (n+1, 0);
+        // for(int i=1; i<=n; i++){
+        //     dp[i] = i;
+        //     for(int j=1; j*j<=i; j++){
+        //         int square = j*j;
+        //         dp[i] = min(dp[i], 1 + dp[i - square]);
+        //     }
+        // }
+        // return dp[n];
     }
 };
