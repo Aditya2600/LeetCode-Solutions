ebbfa2c81cc0ccd72144dc337a4d82689ea83b72
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-21 17:11:00 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2061
          Language: C++
          Runtime: 57 ms
          Runtime percentile: 31.84
          Memory: 12.92 MB
          Memory percentile: 55.93

--- 2061. Painting a Grid With Three Different Colors/Solution.cpp
+++ 2061. Painting a Grid With Three Different Colors/Solution.cpp
@@ -1,78 +1,29 @@
 class Solution {
-private:
-    static constexpr int mod = 1000000007;
-
 public:
+    int MOD = 1e9 + 7;
+    int state_mem[1002][1023];
+    int countWays(int &m, int &n, int r, int c, int curr_state, int prev_state){
+        if(c == n) return 1;
+        if(r == m) return countWays(m, n, 0, c+1, 0, curr_state);
+        if(r == 0 && state_mem[c][prev_state] != -1) return state_mem[c][prev_state];
+
+        int up_color = 0;
+        if(r>0) up_color = curr_state & 3;
+
+        int left_color = (prev_state >> ((m - r - 1)*2)) & 3;
+        int ways_to_color = 0;
+        for(int color = 1; color<=3; ++color){
+            if(color != up_color && color != left_color){
+                ways_to_color = (ways_to_color + countWays(m, n, r+1, c, (curr_state << 2) + color, prev_state))%MOD;    
+            }
+        }
+        if(r == 0){
+            state_mem[c][prev_state] = ways_to_color;
+        }
+        return ways_to_color;
+    }
     int colorTheGrid(int m, int n) {
-        // Hash mapping stores all valid coloration schemes for a single row
-        // that meet the requirements The key represents mask, and the value
-        // represents the ternary string of mask (stored as a list)
-        unordered_map<int, vector<int>> valid;
-
-        // Enumerate masks that meet the requirements within the range [0, 3^m)
-        int mask_end = pow(3, m);
-        for (int mask = 0; mask < mask_end; ++mask) {
-            vector<int> color;
-            int mm = mask;
-            for (int i = 0; i < m; ++i) {
-                color.push_back(mm % 3);
-                mm /= 3;
-            }
-            bool check = true;
-            for (int i = 0; i < m - 1; ++i) {
-                if (color[i] == color[i + 1]) {
-                    check = false;
-                    break;
-                }
-            }
-            if (check) {
-                valid[mask] = move(color);
-            }
-        }
-
-        // Preprocess all (mask1, mask2) binary tuples, satisfying mask1 and
-        // mask2 When adjacent rows, the colors of the two cells in the same
-        // column are different
-        unordered_map<int, vector<int>> adjacent;
-        for (const auto& [mask1, color1] : valid) {
-            for (const auto& [mask2, color2] : valid) {
-                bool check = true;
-                for (int i = 0; i < m; ++i) {
-                    if (color1[i] == color2[i]) {
-                        check = false;
-                        break;
-                    }
-                }
-                if (check) {
-                    adjacent[mask1].push_back(mask2);
-                }
-            }
-        }
-
-        vector<int> f(mask_end);
-        for (const auto& [mask, _] : valid) {
-            f[mask] = 1;
-        }
-        for (int i = 1; i < n; ++i) {
-            vector<int> g(mask_end);
-            for (const auto& [mask2, _] : valid) {
-                for (int mask1 : adjacent[mask2]) {
-                    g[mask2] += f[mask1];
-                    if (g[mask2] >= mod) {
-                        g[mask2] -= mod;
-                    }
-                }
-            }
-            f = move(g);
-        }
-
-        int ans = 0;
-        for (int num : f) {
-            ans += num;
-            if (ans >= mod) {
-                ans -= mod;
-            }
-        }
-        return ans;
+        memset(state_mem, -1, sizeof(state_mem));
+        return countWays(m, n, 0, 0, 0, 0);
     }
 };
