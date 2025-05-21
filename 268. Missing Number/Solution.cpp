f22f044b0cf0e3762378588401455dac49cbe59e
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-21 19:13:08 +0530

    C++ Solution
          Submission Statistics:
          Question #: 268
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 21.78 MB
          Memory percentile: 69.94

--- 268. Missing Number/Solution.cpp
+++ 268. Missing Number/Solution.cpp
@@ -2,18 +2,24 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int i = 0;
-        while(i < n){
-            if(nums[i] < n && nums[i] != nums[nums[i]]){
-                swap(nums[i], nums[nums[i]]);
-            }
-            else i++;
-        }
+        // int i = 0;
+        // while(i < n){
+        //     if(nums[i] < n && nums[i] != nums[nums[i]]){
+        //         swap(nums[i], nums[nums[i]]);
+        //     }
+        //     else i++;
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] != i){
+        //         return i;
+        //     }
+        // }
+        // return n;
+        int actualSum = (n*(n+1))/2;
+        int sum = 0;
         for(int i=0; i<n; i++){
-            if(nums[i] != i){
-                return i;
-            }
+            sum += nums[i];
         }
-        return n;
+        return actualSum - sum;
     }
 };
