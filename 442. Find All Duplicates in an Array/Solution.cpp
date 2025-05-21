5252d69361071c8b07f729978efafe73c5f4ca37
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-21 23:21:13 +0530

    C++ Solution
          Submission Statistics:
          Question #: 442
          Language: C++
          Runtime: 1 ms
          Runtime percentile: 82.07
          Memory: 48.536 MB
          Memory percentile: 61.77

--- 442. Find All Duplicates in an Array/Solution.cpp
+++ 442. Find All Duplicates in an Array/Solution.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> vis(n, false);
-        vector<int> ans;
-        for(auto num : nums){
-            if(!vis[num]){
-                vis[num] = true;
+       int n = nums.size();
+       vector<int> ans;
+        int i = 0;
+        while(i < n){
+            if(nums[i] != nums[nums[i] - 1]){
+                swap(nums[i], nums[nums[i] - 1]);
             }
-            else ans.push_back(num);        
+            else i++;
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i] - 1 != i){
+                ans.push_back(nums[i]);
+            }
         }
         return ans;
+        // vector<bool> vis(n, false);
+        // vector<int> ans;
+        // for(auto num : nums){
+        //     if(!vis[num]){
+        //         vis[num] = true;
+        //     }
+        //     else ans.push_back(num);        
+        // }
+        // return ans;
+
     }
 };
