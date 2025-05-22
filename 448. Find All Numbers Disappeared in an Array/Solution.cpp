bd1c37beb112e7e371f5fd851e6667f08db70958
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-22 09:15:07 +0530

    C++ Solution
          Submission Statistics:
          Question #: 448
          Language: C++
          Runtime: 6 ms
          Runtime percentile: 53.83
          Memory: 53.112 MB
          Memory percentile: 53.54

--- 448. Find All Numbers Disappeared in an Array/Solution.cpp
+++ 448. Find All Numbers Disappeared in an Array/Solution.cpp
@@ -3,18 +3,31 @@ public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans;
-        int i = 0;
-        while(i < n){
-            if(nums[i] != nums[nums[i]-1]){
-                swap(nums[i], nums[nums[i]-1]);
-            }
-            else i++;
-        }
-        for(int i=0; i<n; i++){
-            if(i != nums[i] - 1){
-                ans.push_back(i+1);
+
+        for (int i = 0; i < n; i++) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] *= -1;
             }
         }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                ans.push_back(i + 1); // these numbers are missing
+            }
+        }
+        // int i = 0;
+        // while(i < n){
+        //     if(nums[i] != nums[nums[i]-1]){
+        //         swap(nums[i], nums[nums[i]-1]);
+        //     }
+        //     else i++;
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(i != nums[i] - 1){
+        //         ans.push_back(i+1);
+        //     }
+        // }
         return ans;
     }
 };
