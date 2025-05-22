b700a51b0eeff847169e20f80890e91c8a9dc8d3
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-22 08:10:10 +0530

    C++ Solution
          Submission Statistics:
          Question #: 448
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 82.13
          Memory: 53.108 MB
          Memory percentile: 53.54

---
+++ 448. Find All Numbers Disappeared in an Array/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        int i = 0;
+        while(i < n){
+            if(nums[i] != nums[nums[i]-1]){
+                swap(nums[i], nums[nums[i]-1]);
+            }
+            else i++;
+        }
+        for(int i=0; i<n; i++){
+            if(i != nums[i] - 1){
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
+    }
+};
