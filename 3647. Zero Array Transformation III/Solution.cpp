d3fa167cece9990c6f6813a34368df84d922a80c
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-23 11:40:19 +0530

    C++ Solution
          Submission Statistics:
          Question #: 3647
          Language: C++
          Runtime: 139 ms
          Runtime percentile: 22.90
          Memory: 224.408 MB
          Memory percentile: 37.63

--- 3647. Zero Array Transformation III/Solution.cpp
+++ 3647. Zero Array Transformation III/Solution.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
-        sort(queries.begin(), queries.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[0] < b[0];
-             });
-
-        priority_queue<int> heap;
-        vector<int> deltaArray(nums.size() + 1, 0);
-        int operations = 0;
-        for (int i = 0, j = 0; i < nums.size(); ++i) {
-            operations += deltaArray[i];
-            while (j < queries.size() && queries[j][0] == i) {
-                heap.push(queries[j][1]);
-                ++j;
+        sort(queries.begin(), queries.end());
+        int ans = 0;
+        int j = 0;
+        int n = nums.size();
+        priority_queue<int> candidates;
+        priority_queue<int, vector<int>, greater<int>> chosen;
+        
+        for(int i=0; i<n; i++){
+            while(j < queries.size() && queries[j][0] == i){
+                candidates.push(queries[j][1]);
+                j++;
             }
-            while (operations < nums[i] && !heap.empty() && heap.top() >= i) {
-                operations += 1;
-                deltaArray[heap.top() + 1] -= 1;
-                heap.pop();
+            nums[i] -= (int)chosen.size();
+            while(nums[i] > 0 && !candidates.empty() && candidates.top() >= i){
+                ans++;
+                chosen.push(candidates.top());
+                candidates.pop();
+                nums[i]--;
             }
-            if (operations < nums[i]) {
+            if(nums[i] > 0){
                 return -1;
             }
+            while(!chosen.empty() && chosen.top() == i){
+                chosen.pop();
+            }           
         }
-        return heap.size();
+        return queries.size() - ans;
     }
 };
