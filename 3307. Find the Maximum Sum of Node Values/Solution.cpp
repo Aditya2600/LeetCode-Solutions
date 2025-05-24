af049413de89a6f3fa9b84bb766ca40ff3f613cc
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-24 11:06:21 +0530

    C++ Solution
          Submission Statistics:
          Question #: 3307
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 54.90
          Memory: 126.74 MB
          Memory percentile: 50.33

--- 3307. Find the Maximum Sum of Node Values/Solution.cpp
+++ 3307. Find the Maximum Sum of Node Values/Solution.cpp
@@ -1,29 +1,21 @@
+#define ll long long int
+
 class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        long long total = 0;
-        int count = 0;
-        int minDiff = INT_MAX;
-
-        for (int num : nums) {
-            int x = num ^ k;
-            // If applying XOR increases value, count it
-            if (x > num) {
-                total += x;
-                count++;
-            } else {
-                total += num;
+        ll sum = 0, cnt = 0, balikabakra = INT_MAX;
+        for(auto el : nums){
+            if((el ^ k) > el){
+                sum += el^k;
+                balikabakra = min(balikabakra, (ll)(el^k) - el);
+                cnt++;
             }
-
-            // Track the minimal difference caused by XOR operation
-            minDiff = min(minDiff, abs(x - num));
+            else{
+                sum += el;
+                balikabakra = min(balikabakra, (ll)el - (el^k));
+            }
         }
-
-        // If odd number of beneficial XORs, one must be reverted (to keep edge pairing possible)
-        if (count % 2 != 0) {
-            total -= minDiff;
-        }
-
-        return total;
+        if(cnt % 2 == 0) return sum;
+        return sum - balikabakra;
     }
 };
