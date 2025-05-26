a62e6507a707af8a5679a0e0c7e32cc9f65db16a
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-26 12:13:18 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1527
          Language: C++
          Runtime: 35 ms
          Runtime percentile: 60.80
          Memory: 18.712 MB
          Memory percentile: 24.42

--- 1527. Number of Ways to Paint N × 3 Grid/Solution.cpp
+++ 1527. Number of Ways to Paint N × 3 Grid/Solution.cpp
@@ -1,14 +1,63 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        long c3 = 6;
-        long c2 = 6;
-        long mod = 1e9 + 7;
-        for(int i=2; i<=n; ++i){
-            long temp = c3;
-            c3 = (2*c2 + 2*c3)%mod;
-            c2 = (2*temp + 3*c2)%mod;
+    const int MOD = 1e9 + 7;
+    vector<vector<int>> patterns;
+    vector<vector<int>> comp;
+    
+    void dfs(int row, int m, vector<int> cur){
+        if(row == m){
+            patterns.push_back(cur);
+            return;
         }
-        return (c3 + c2)%mod;
+
+        for(int i = 0; i < 3; i++){
+            if(row > 0 && cur[row - 1] == i) continue;
+            cur[row] = i;
+            dfs(row + 1, m, cur);
+        }
     }
-};
+
+    void buildComp(){
+        int k = patterns.size();
+        comp.resize(k);
+
+        for(int i = 0; i < k; i++){
+            for(int j = 0; j < k; j++){
+                bool f = true;
+                for(int r = 0; r < (int)patterns[i].size(); r++){
+                    if(patterns[i][r] == patterns[j][r]){
+                        f = false;
+                        break;
+                    }
+                }
+                if(f) comp[i].push_back(j); 
+            }
+        }
+    }
+
+    int solve(vector<vector<int>>& dp, int col, int prevCol, int n){
+        if(col == n) return 1;
+        if(dp[col][prevCol + 1] != -1) return dp[col][prevCol + 1];
+
+        long long ways = 0;
+        if(prevCol == -1){
+            for(int i = 0; i < (int)patterns.size(); i++){
+                ways = (ways + solve(dp, col + 1, i, n)) % MOD;
+            }
+        }
+        else{
+            for(auto colu : comp[prevCol]){
+                ways = (ways + solve(dp, col + 1, colu, n)) % MOD;
+            }
+        }
+
+        return dp[col][prevCol + 1] = ways;
+    }
+    int numOfWays(int n) {
+        vector<int> cur(3);
+        dfs(0, 3, cur);
+        buildComp();
+        vector<vector<int>> dp(n, vector<int>(patterns.size() + 1, -1));
+        return solve(dp, 0, -1, n);
+    }
+}; 
