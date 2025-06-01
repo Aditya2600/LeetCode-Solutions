69d548b0537a0917c41ac9a3099c8d9598b90873
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-01 11:01:40 +0530

    C++ Solution
          Submission Statistics:
          Question #: 945
          Language: C++
          Runtime: 4 ms
          Runtime percentile: 50.31
          Memory: 17.636 MB
          Memory percentile: 40.41

---
+++ 945. Snakes and Ladders/Solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size(), label = 1;
+        vector<pair<int, int>> cells(n*n+1);
+        vector<int> columns(n);
+        for(int i=0; i<n; i++) columns[i] = i;
+        for(int row = n-1; row >= 0; row--){
+            for(int col : columns){
+                cells[label++] = {row, col};
+            }
+            reverse(columns.begin(), columns.end());
+        }
+        vector<int> dist(n*n+1, -1);
+        dist[1] = 0;
+        queue<int> q;
+        q.push(1);
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            for(int next = curr + 1; next <= min(curr + 6, n*n); next++){
+                auto [row, col] = cells[next];
+                int destination = board[row][col] != -1 ? board[row][col] : next;
+                if(dist[destination] == -1){
+                    dist[destination] = 1 + dist[curr];
+                    q.push(destination);
+                }
+            }
+        }
+        return dist[n*n];
+    }
+};
