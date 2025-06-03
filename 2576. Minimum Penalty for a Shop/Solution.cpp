105ec8e0d46a5d6aca505bf5489d4b49f7c3f1eb
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-03 09:45:52 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2576
          Language: C++
          Runtime: 14 ms
          Runtime percentile: 22.54
          Memory: 21.812 MB
          Memory percentile: 15.54

--- 2576. Minimum Penalty for a Shop/Solution.cpp
+++ 2576. Minimum Penalty for a Shop/Solution.cpp
@@ -2,28 +2,48 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int n = customers.size();
-        int cntY = 0;
+        vector<int> pre(n+1, 0);
+        vector<int> suff(n+1, 0);
         for(int i=0; i<n; i++){
-            if(customers[i] == 'Y'){
-                cntY++;
-            }
+            pre[i+1] = pre[i] + (customers[i] == 'N' ? 1 : 0);
         }
-        if(cntY == 0) return 0;
-        vector<int> ans(n+1, cntY);
-        for(int i=1; i<=n; i++){
-            if(customers[i - 1] == 'Y'){
-                ans[i] = ans[i-1] - 1;
-            }
-            else ans[i] = ans[i-1] + 1;
+        for(int i=n-1; i>=0; i--){
+            suff[i] = suff[i+1] + (customers[i] == 'Y' ? 1 : 0);
         }
+        int minPenalty = n;
         int ind = 0;
-        int minHour = INT_MAX;
-        for(int i = 0; i<=n; i++){
-            if(ans[i] < minHour){
-                minHour = ans[i];
+        for(int i=0; i<=n; i++){
+            pre[i] += suff[i];
+            if(minPenalty > pre[i]){
+                minPenalty = pre[i];
                 ind = i;
-            }   
+                
+            }
         }
         return ind;
+        // int n = customers.size();
+        // int cntY = 0;
+        // for(int i=0; i<n; i++){
+        //     if(customers[i] == 'Y'){
+        //         cntY++;
+        //     }
+        // }
+        // if(cntY == 0) return 0;
+        // vector<int> ans(n+1, cntY);
+        // for(int i=1; i<=n; i++){
+        //     if(customers[i - 1] == 'Y'){
+        //         ans[i] = ans[i-1] - 1;
+        //     }
+        //     else ans[i] = ans[i-1] + 1;
+        // }
+        // int ind = 0;
+        // int minHour = INT_MAX;
+        // for(int i = 0; i<=n; i++){
+        //     if(ans[i] < minHour){
+        //         minHour = ans[i];
+        //         ind = i;
+        //     }   
+        // }
+        // return ind;
     }
 };
