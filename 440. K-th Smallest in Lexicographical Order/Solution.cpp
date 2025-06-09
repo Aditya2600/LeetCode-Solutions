bb497c5eeb1c66f353c499819aa732ba7e4fc883
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-09 23:59:50 +0530

    C++ Solution
          Submission Statistics:
          Question #: 440
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 7.764 MB
          Memory percentile: 83.39

--- 440. K-th Smallest in Lexicographical Order/Solution.cpp
+++ 440. K-th Smallest in Lexicographical Order/Solution.cpp
@@ -1,31 +1,29 @@
 class Solution {
+    #define ll long long
 public:
+    int countSteps(int n, ll prefix1, ll prefix2){
+        int steps = 0;
+        while(prefix1<=n){
+            steps += min((ll)n+1, prefix2) - prefix1; 
+            prefix1 *= 10;
+            prefix2 *= 10;
+        }
+        return steps;
+    }
     int findKthNumber(int n, int k) {
         int curr = 1;
-        k--; // we already count 1 as the first number
-
-        while (k > 0) {
-            long long steps = countSteps(n, curr, curr + 1);
-            if (steps <= k) {
-                // Skip entire subtree
-                curr += 1;
+        k -= 1;
+        while(k){
+            int steps = countSteps(n, curr, curr+1);
+            if(steps <= k){
+                curr++;
                 k -= steps;
-            } else {
-                // Go deeper into the subtree
+            }
+            else{
                 curr *= 10;
                 k--;
             }
         }
         return curr;
-    }
-
-    long long countSteps(int n, long long curr, long long next) {
-        long long steps = 0;
-        while (curr <= n) {
-            steps += min((long long)n + 1, next) - curr;
-            curr *= 10;
-            next *= 10;
-        }
-        return steps;
     }
 };
