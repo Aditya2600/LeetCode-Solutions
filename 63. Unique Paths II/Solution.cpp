23db021d26d2fe1c8fc1c83d2f9ce4f681093fc5
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-11 06:12:53 +0530

    C++ Solution
          Submission Statistics:
          Question #: 63
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 11.248 MB
          Memory percentile: 95.47

--- 63. Unique Paths II/Solution.cpp
+++ 63. Unique Paths II/Solution.cpp
@@ -1,20 +1,39 @@
 class Solution {
-public: 
-    int dp[101][101];
-    int f(int i, int j, vector<vector<int>>& obstacleGrid){
+public:
+    long long dp[101][101];
+    // int f(int i, int j, vector<vector<int>>& obstacleGrid){
+    //     int n = obstacleGrid.size();
+    //     int m = obstacleGrid[0].size();
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(i == n-1 && j == m-1 && obstacleGrid[n-1][m-1] != 1) return 1;
+    //     if(i < 0 || i >= n || j < 0 || j >= m) return 0;
+    //     if(obstacleGrid[i][j] == 1) return 0;
+    //     int ans = 0;
+    //     ans = f(i+1, j, obstacleGrid) + f(i, j+1, obstacleGrid);
+    //     return dp[i][j] = ans;
+
+    // }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        if(dp[i][j] != -1) return dp[i][j];
-        if(i == n-1 && j == m-1 && obstacleGrid[n-1][m-1] != 1) return 1;
-        if(i < 0 || i >= n || j < 0 || j >= m) return 0;
-        if(obstacleGrid[i][j] == 1) return 0;
-        int ans = 0;
-        ans = f(i+1, j, obstacleGrid) + f(i, j+1, obstacleGrid);
-        return dp[i][j] = ans;
-        
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        memset(dp, -1, sizeof dp);
-        return f(0, 0, obstacleGrid);
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1) {
+            return 0;
+        }
+
+        memset(dp, 0, sizeof dp);
+        dp[n - 1][m - 1] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0; // obstacle cell
+                } else {
+                    if (i + 1 < n)
+                        dp[i][j] += dp[i + 1][j];
+                    if (j + 1 < m)
+                        dp[i][j] += dp[i][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
