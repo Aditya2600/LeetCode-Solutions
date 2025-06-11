0af9e1a4f3e4f7bbe373d3ab699e2804053ca12a
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-11 07:41:10 +0530

    C++ Solution
          Submission Statistics:
          Question #: 64
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 15.468 MB
          Memory percentile: 83.39

--- 64. Minimum Path Sum/Solution.cpp
+++ 64. Minimum Path Sum/Solution.cpp
@@ -10,7 +10,22 @@ public:
         return dp[i][j] = grid[i][j] + min(f(i+1, j, grid), f(i, j+1, grid));
     }
     int minPathSum(vector<vector<int>>& grid) {
-        memset(dp, -1, sizeof(dp));
-        return f(0, 0, grid);
+        memset(dp, 0, sizeof(dp));
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
+                if(i == n-1 && j == m-1){
+                    dp[n-1][m-1] = grid[n-1][m-1];
+                }
+                else{
+                    int right = (j + 1 < m) ? dp[i][j+1] : INT_MAX;
+                    int down = (i + 1 < n) ? dp[i+1][j] : INT_MAX;
+                    dp[i][j] = grid[i][j] + min(right, down);
+                }                                                             
+            }
+        }
+        return dp[0][0];
     }
 };
