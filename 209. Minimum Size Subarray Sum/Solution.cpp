e76de3c70a4cd7f793d617225185291bbe6f7617
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-13 10:34:48 +0530

    C++ Solution
          Submission Statistics:
          Question #: 209
          Language: C++
          Runtime: 15 ms
          Runtime percentile: 5.24
          Memory: 33.06 MB
          Memory percentile: 5.21

--- 209. Minimum Size Subarray Sum/Solution.cpp
+++ 209. Minimum Size Subarray Sum/Solution.cpp
@@ -2,31 +2,54 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-        int i = 0;
-        int j = 0;
-        int sum = nums[0];
-        int len = n;
-        while(j<n && i<n){
-            if(sum < target && (j-i+1) == n){
-                len = 0;
-                break;
-            }
-            if(sum < target){
-                j++;
-                if(j == n){
-                    break;
-                }
-                else{
-                    sum += nums[j];
-                }
-                
-            }
-            if(sum >= target){
-                len = min(len, j-i+1);
-                sum -= nums[i];
-                i++;
+        int len = INT_MAX;
+        vector<int> pre(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            pre[i] = pre[i - 1] + nums[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            int lo = i;
+            int hi = n;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                int sum = pre[mid] - pre[i - 1];
+                if (sum >= target) {
+                    len = min(len, mid - (i - 1));
+                    hi = mid - 1;
+                } else
+                    lo = mid + 1;
             }
         }
-        return  len;
+        if(len == INT_MAX) return 0;
+    return len;
     }
-};
+    // int n = nums.size();
+    // int i = 0;
+    // int j = 0;
+    // int sum = nums[0];
+    // int len = n;
+    // while(j<n && i<n){
+    //     if(sum < target && (j-i+1) == n){
+    //         len = 0;
+    //         break;
+    //     }
+    //     if(sum < target){
+    //         j++;
+    //         if(j == n){
+    //             break;
+    //         }
+    //         else{
+    //             sum += nums[j];
+    //         }
+
+    //     }
+    //     if(sum >= target){
+    //         len = min(len, j-i+1);
+    //         sum -= nums[i];
+    //         i++;
+    //     }
+    // }
+    // return  len;
+
+}
+;
