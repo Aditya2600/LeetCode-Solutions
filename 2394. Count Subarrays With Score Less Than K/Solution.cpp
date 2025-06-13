3395ea652cc8dcb8410709c0e5294e9a21af1b9b
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-13 16:46:23 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2394
          Language: C++
          Runtime: 2 ms
          Runtime percentile: 61.87
          Memory: 99.04 MB
          Memory percentile: 44.25

--- 2394. Count Subarrays With Score Less Than K/Solution.cpp
+++ 2394. Count Subarrays With Score Less Than K/Solution.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        long long res = 0, sum = 0;
-        int left = 0;
-        for (int right = 0; right < nums.size(); ++right) {
-            sum += nums[right];
-            while (sum * (right - left + 1) >= k) {
-                sum -= nums[left++];
+        long long n = nums.size(), sum = 0, i = 0, j = 0, res = 0, score = 0;
+        while (j < n) {
+            sum += nums[j];
+            score = sum * (j-i+1);
+            while (score >= k && i<=j) {
+                score = sum / (j-i+1);
+                sum -= nums[i];
+                score = sum * ((j-i+1) - 1);
+                i++;
             }
-            res += (right - left + 1);
+            if((j-i+1) > 0)score = score / (j-i+1);
+            res += (j-i+1);
+            j++;
         }
-        return res;
+    return res;
     }
 };
