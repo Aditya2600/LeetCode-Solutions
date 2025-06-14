cc741ea3568b42112605e076427209dc97a8ecfe
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-14 19:58:09 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2704
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 8.396 MB
          Memory percentile: 18.49

---
+++ 2704. Maximum Difference by Remapping a Digit/Solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s1 = to_string(num);
+        string s2 = to_string(num);
+        int value1, value2;
+        int k1 = 1;
+        int k2 = 1;
+        for(int i=0; i<s1.size(); i++){
+            if(s1[i] != '9' && k1>0){
+                value1 = s1[i];
+                s1[i] = '9';
+                k1--;
+            }
+            if(s1[i] == value1){
+                s1[i] = '9';
+            }
+        }
+        for(int i=0; i<s2.size(); i++){
+            if(s2[i] != '0' && k2>0){
+                value2 = s2[i];
+                s2[i] = '0';
+                k2--;
+            }
+            if(s2[i] == value2){
+                s2[i] = '0';
+            }
+        }
+        int ans;
+        if(!s2.empty()){
+             ans = stoi(s1) - stoi(s2);
+        }
+        return ans;
+    }
+};
