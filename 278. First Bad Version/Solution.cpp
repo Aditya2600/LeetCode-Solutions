951cec0e90f5fd2cec05d7b0a0eb37479520fee8
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-14 09:10:55 +0530

    C++ Solution
          Submission Statistics:
          Question #: 278
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 7.676 MB
          Memory percentile: 99.60

---
+++ 278. First Bad Version/Solution.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n;
+        int mid;
+        int ans;
+        while(lo<=hi){
+            mid = lo + (hi-lo)/2;
+            if(isBadVersion(mid)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else lo = mid + 1;
+        }
+        return ans;
+    }
+};
