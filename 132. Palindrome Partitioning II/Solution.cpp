9c018472b4e9624e8b066283c6a9847738240289
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-19 19:32:41 +0530

    C++ Solution
          Submission Statistics:
          Question #: 132
          Language: C++
          Runtime: 2041 ms
          Runtime percentile: 5.02
          Memory: 9.2 MB
          Memory percentile: 93.84

--- 132. Palindrome Partitioning II/Solution.cpp
+++ 132. Palindrome Partitioning II/Solution.cpp
@@ -8,24 +8,37 @@ public:
         }
         return true;
     }
-    int f(int i, string &s, vector<int>& dp, int n){
-        if(i == s.size()){
-            return 0;
-        }
-        int minCost = INT_MAX;
-        if(dp[i] != -1) return dp[i];
-        for(int j=i; j<s.size(); j++){
-            if(isPalindrome(s, i, j)){
-                int cost = 1 + f(j+1, s, dp, n);
-                minCost = min(minCost, cost);
-            }
-        }
+    // int f(int i, string &s, vector<int>& dp, int n){
+    //     if(i == s.size()){
+    //         return 0;
+    //     }
+    //     int minCost = INT_MAX;
+    //     if(dp[i] != -1) return dp[i];
+    //     for(int j=i; j<s.size(); j++){
+    //         if(isPalindrome(s, i, j)){
+    //             int cost = 1 + f(j+1, s, dp, n);
+    //             minCost = min(minCost, cost);
+    //         }
+    //     }
         
-        return dp[i] = minCost;
-    }
+    //     return dp[i] = minCost;
+    // }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, s, dp, n) - 1;
+        vector<int> dp(n+1, 0);
+        dp[n] = 0;
+        for(int i = n-1; i>=0; i--){
+        int minCost = INT_MAX;
+            for(int j=i; j<n; j++){
+                if(isPalindrome(s, i, j)){
+                    int cost = 1 + dp[j+1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
+        // vector<int> dp(n, -1);
+        // return f(0, s, dp, n) - 1;
     }
 };
