d6920167f828e4a8d09d3beef0cc27860092c6f5
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-20 14:11:47 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1403
          Language: C++
          Runtime: 204 ms
          Runtime percentile: 10.36
          Memory: 113.52 MB
          Memory percentile: 8.67

--- 1403. Palindrome Partitioning III/Solution.cpp
+++ 1403. Palindrome Partitioning III/Solution.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    int c(int i, int j, const string &s) {
+    int c(int i, int j, string& s) {
         int cnt = 0;
         while (i < j) {
-            if (s[i] != s[j]) {
+            if (s[i++] != s[j--]) {
                 cnt++;
             }
-            i++;
-            j--;
         }
         return cnt;
     }
-
-    int f(int i, int cnt, int n, int k, const string &s, vector<vector<int>> &dp) {
-        if (i == n) {
-            if (cnt == k) {
-                return 0;
-            } else {
-                return 100000;
-            }
+    int f(int i, string s, int k, int cnt, int n, vector<vector<int>>& dp) {
+        if (i == n && cnt == k) {
+            return 0;
         }
-        if (dp[i][cnt] != -1) {
-            return dp[i][cnt];
-        }
+        if(dp[i][cnt] != -1) return dp[i][cnt];
         int ans = 100000;
-        for (int j = i; j < n; j++) {
-            ans = min(ans, c(i, j, s) + f(j + 1, cnt + 1, n, k, s, dp));
+        for(int j=i; j<n; j++){
+            ans = min(ans, c(i, j, s) + f(j+1, s, k, cnt+1, n, dp));
         }
         return dp[i][cnt] = ans;
     }
-
     int palindromePartition(string s, int k) {
         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return f(0, 0, n, k, s, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return f(0, s, k, 0, n, dp);
     }
 };
