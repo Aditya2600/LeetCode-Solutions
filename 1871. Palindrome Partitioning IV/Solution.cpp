d5fa79e69a8421e97bbc06993a6d94b4a70d4ee8
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-20 22:37:56 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1871
          Language: C++
          Runtime: 44 ms
          Runtime percentile: 95.54
          Memory: 20.604 MB
          Memory percentile: 51.49

--- 1871. Palindrome Partitioning IV/Solution.cpp
+++ 1871. Palindrome Partitioning IV/Solution.cpp
@@ -1,34 +1,61 @@
 class Solution {
 public:
+    // bool isPalindrome(int i, int j, string &s){
+    //     while(i < j){
+    //         if(s[i++] != s[j--]){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // bool f(int i, string &s, int n, int cnt, vector<vector<long long>>& dp){
+    //     if(i == n){
+    //         return cnt == 3;
+    //     }
+    //     if(dp[i][cnt] != -1) return dp[i][cnt];
+    //     for(int j=i; j<n; j++){
+    //         if(isPalindrome(i, j, s)){
+    //             if(f(j+1, s, n, cnt+1, dp)){
+    //                 return dp[i][cnt] = true;
+    //             }
+    //         }
+    //     }
+    //     return dp[i][cnt] = false;
+    // }
     bool checkPartitioning(string s) {
         int n = s.size();
-
-        // Step 1: Precompute all palindromic substrings
-        vector<vector<bool>> isPal(n, vector<bool>(n, false));
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-                if (s[i] == s[j] && (len <= 2 || isPal[i + 1][j - 1])) {
-                    isPal[i][j] = true;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int i=0; i<n; i++){
+            dp[i][i] = true;
+            int l = i - 1;
+            int r = i + 1;
+            while(l >= 0 && r < n && s[l] == s[r]){
+                dp[l][r] = true;
+                l--;
+                r++;
+            }
+        }
+        for(int i=0; i<n-1; i++){
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = true;
+                int l = i-1;
+                int r = i+2;
+                while(l >= 0 && r < n && s[l] == s[r]){
+                    dp[l][r] = true;
+                    l--;
+                    r++;
                 }
             }
         }
-
-        // Step 2: Bottom-up DP
-        vector<vector<bool>> dp(n + 1, vector<bool>(4, false));
-        dp[n][0] = true; // base case: 0 palindromic parts needed beyond end
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int k = 1; k <= 3; k++) { // need to partition into k palindromic parts
-                for (int j = i; j < n; j++) {
-                    if (isPal[i][j] && dp[j + 1][k - 1]) {
-                        dp[i][k] = true;
-                        break; // no need to check further
+        for(int i=0; i<n; i++){
+            if(dp[0][i]){
+                for(int j=i+1; j<n-1; j++){
+                    if(dp[i+1][j] && dp[j+1][n-1]){
+                        return true;
                     }
                 }
             }
         }
-
-        return dp[0][3];
+        return false;
     }
 };
