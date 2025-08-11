a89dff2e39c3db0dccca51969827b9b5b5c76046
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-11 19:47:37 +0530

    C++ Solution
          Submission Statistics:
          Question #: 5
          Language: C++
          Runtime: 175 ms
          Runtime percentile: 25.97
          Memory: 13.888 MB
          Memory percentile: 52.55

---
+++ 5. Longest Palindromic Substring/Solution.cpp
@@ -0,0 +1,61 @@
+// class Solution {
+// private:
+//     bool isPalindrome(const string &s){
+//         int l = 0;
+//         int r = s.length() - 1;
+//         while(l < r){
+//             if(s[l] != s[r]){
+//                 return false;
+//             }
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
+// public:
+//     string longestPalindrome(string s) {
+//         if(s.length() <= 1){
+//             return s;
+//         }
+//         int max_len = 1;
+//         string max_str = s.substr(0, 1);
+//         for(int i=0; i<s.length(); i++){
+//             for(int j=i+max_len; j<=s.length(); j++){
+//                 if(j-i > max_len && isPalindrome(s.substr(i, j-i))){
+//                     max_len = j-i;
+//                     max_str = s.substr(i, j-i);
+//                 }
+//             }
+//         }
+//         return max_str;
+//     }
+// };
+class Solution {
+public:
+    int dp[1001][1001];
+    bool solve(string &s, int l, int r){
+        if(l >= r){
+            return true;
+        }
+        if(dp[l][r] != -1) return dp[l][r];
+        if(s[l] == s[r]){
+            return dp[l][r] = solve(s, l+1, r-1);
+        }
+        return dp[l][r] = false;
+    }
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int max_len = INT_MIN;
+        int startingIndex = 0;
+        memset(dp, -1, sizeof(dp));
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(solve(s, i, j) && max_len < j-i+1){
+                    startingIndex = i;
+                    max_len = j-i+1;
+                }
+            }
+        }
+        return s.substr(startingIndex, max_len);
+    }
+};
