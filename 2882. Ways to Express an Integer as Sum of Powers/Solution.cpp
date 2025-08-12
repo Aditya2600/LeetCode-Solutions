228d4da5b5fd7941df9816dcb318df88da2b5839
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-12 13:00:48 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2882
          Language: C++
          Runtime: 106 ms
          Runtime percentile: 68.19
          Memory: 10.748 MB
          Memory percentile: 89.11

---
+++ 2882. Ways to Express an Integer as Sum of Powers/Solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+int dp[302][302];
+#define MOD 1000000007
+vector<int> powers;
+
+    int binaryExponentiation(int a, int b){
+        int res = 1;
+        while(b){
+            if(b & 1){
+                res *= a;
+            }
+            a *= a;
+            b >>= 1;
+        }
+        return res;
+    }
+    int findWays(int pos, int target){
+        if(target == 0) return 1;
+        if(pos == powers.size() || target < 0) return 0;
+        if(dp[pos][target] != -1) return dp[pos][target];
+        int include = findWays(pos + 1, target - powers[pos]);
+        int exclude = findWays(pos + 1, target);
+        return dp[pos][target] = (include + exclude) % MOD;
+    }
+public:
+    int numberOfWays(int n, int x) {
+        for(int i=1; i<=n; i++){
+            int power = binaryExponentiation(i, x);
+            if(power > n) break;
+            powers.push_back(power);
+        }
+        memset(dp, -1, sizeof(dp));
+        return findWays(0, n);
+    }
+};
