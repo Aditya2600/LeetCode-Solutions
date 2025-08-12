ac6bb2e1bee0ea13dc71dde6d483f724d4331f6d
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-12 18:56:26 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2882
          Language: C++
          Runtime: 71 ms
          Runtime percentile: 76.25
          Memory: 73.592 MB
          Memory percentile: 50.11

--- 2882. Ways to Express an Integer as Sum of Powers/Solution.cpp
+++ 2882. Ways to Express an Integer as Sum of Powers/Solution.cpp
@@ -14,22 +14,46 @@ vector<int> powers;
         }
         return res;
     }
-    int findWays(int pos, int target){
-        if(target == 0) return 1;
-        if(pos == powers.size() || target < 0) return 0;
-        if(dp[pos][target] != -1) return dp[pos][target];
-        int include = findWays(pos + 1, target - powers[pos]);
-        int exclude = findWays(pos + 1, target);
-        return dp[pos][target] = (include + exclude) % MOD;
-    }
+//     int findWays(int pos, int target){
+//         if(target == 0) return 1;
+//         if(pos == powers.size() || target < 0) return 0;
+//         if(dp[pos][target] != -1) return dp[pos][target];
+//         int include = findWays(pos + 1, target - powers[pos]);
+//         int exclude = findWays(pos + 1, target);
+//         return dp[pos][target] = (include + exclude) % MOD;
+//     }
+// public:
+//     int numberOfWays(int n, int x) {
+//         for(int i=1; i<=n; i++){
+//             int power = binaryExponentiation(i, x);
+//             if(power > n) break;
+//             powers.push_back(power);
+//         }
+//         memset(dp, -1, sizeof(dp));
+//         return findWays(0, n);
+//     }
 public:
     int numberOfWays(int n, int x) {
+        vector<int> powers;
         for(int i=1; i<=n; i++){
             int power = binaryExponentiation(i, x);
             if(power > n) break;
             powers.push_back(power);
         }
-        memset(dp, -1, sizeof(dp));
-        return findWays(0, n);
+        int p = powers.size();
+        vector<vector<int>> dp(p+1, vector<int>(n+1, 0));
+        for(int pos=1; pos<=p; pos++){
+            dp[pos-1][0] = 1;
+            for(int target = 1; target<=n; target++){
+                if(powers[pos-1]>target){
+                    dp[pos][target] = dp[pos-1][target];
+                }
+                else{
+                    dp[pos][target] = (dp[pos-1][target] + dp[pos-1][target-powers[pos-1]]) % MOD;
+                }
+            }
+            
+        }
+        return dp[p][n];
     }
 };
