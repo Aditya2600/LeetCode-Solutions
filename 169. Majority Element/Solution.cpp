b29648d8943b80890680296b40f411632baef767
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-13 21:11:33 +0530

    C++ Solution
          Submission Statistics:
          Question #: 169
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 28.072 MB
          Memory percentile: 86.85

--- 169. Majority Element/Solution.cpp
+++ 169. Majority Element/Solution.cpp
@@ -1,15 +1,44 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+//         int n = nums.size();
+//         int ans = 0;
+//         for(int i=0; i<n; i++){
+//             mp[nums[i]]++;
+//             if(mp[nums[i]] > n/2){
+//                 ans = nums[i];
+//             }
+//         }
+//     return ans;
+//     }
+// };
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int n = nums.size();
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]] > n/2){
-                ans = nums[i];
+       int ele;
+       int cnt = 0;
+       for(int i=0; i<nums.size(); i++){
+            if(cnt == 0){
+                ele = nums[i];
+                cnt++;
             }
-        }
-    return ans;
+            else if(nums[i] == ele){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+       }
+       int cnt1 = 0;
+       for(int i=0; i<nums.size(); i++){
+            if(nums[i] == ele){
+                cnt1++;
+            }
+       }
+       if(cnt1 > nums.size()/2){
+        return ele;
+       }
+       return -1;
     }
 };
