27f8419dea180511c66284191d64fa5b5c5ec2d8
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-14 08:47:37 +0530

    C++ Solution
          Submission Statistics:
          Question #: 229
          Language: C++
          Runtime: 4 ms
          Runtime percentile: 49.06
          Memory: 23.556 MB
          Memory percentile: 80.33

--- 229. Majority Element II/Solution.cpp
+++ 229. Majority Element II/Solution.cpp
@@ -14,19 +14,61 @@
 //     return ans;
 //     }
 // };
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ls;
+//         int min = n / 3 + 1;
+//         unordered_map<int, int> mp;
+//         for (int i = 0; i < n; i++) {
+//             mp[nums[i]]++;
+//             if (mp[nums[i]] == min) {
+//                 ls.push_back(nums[i]);
+//             }
+//         }
+//         return ls;
+//     }
+// };
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ls;
-        int min = n / 3 + 1;
-        unordered_map<int, int> mp;
-        for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-            if (mp[nums[i]] == min) {
-                ls.push_back(nums[i]);
+        int ele1 = INT_MIN;
+        int ele2 = INT_MIN;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(cnt1 == 0 &&  ele2 != nums[i]){
+                ele1 = nums[i];
+                cnt1 = 1;
             }
+            else if(cnt2 == 0 &&  ele1 != nums[i]){
+                ele2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(nums[i] == ele1){
+                cnt1++;
+            }
+            else if(nums[i] == ele2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+
         }
-        return ls;
+        vector<int> ans;
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(ele1 == nums[i]) cnt1++;
+            if(ele2 == nums[i]) cnt2++;
+        }
+        int mini = (int)(n/3)+1;
+        if(cnt1 >= mini) ans.push_back(ele1);
+        if(cnt2 >= mini) ans.push_back(ele2);
+        return ans;
     }
 };
