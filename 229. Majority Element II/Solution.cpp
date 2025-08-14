45c7f81237781c699b6434a2d39aeaa620ece8f3
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-14 07:10:01 +0530

    C++ Solution
          Submission Statistics:
          Question #: 229
          Language: C++
          Runtime: 9 ms
          Runtime percentile: 28.06
          Memory: 26.428 MB
          Memory percentile: 18.27

--- 229. Majority Element II/Solution.cpp
+++ 229. Majority Element II/Solution.cpp
@@ -1,15 +1,32 @@
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+//         vector<int> ans;
+//         int n = nums.size();
+//         for(int i=0; i<n; i++){
+//             mp[nums[i]]++;
+//             if(find(ans.begin(), ans.end(), nums[i]) == ans.end() &&
+//             mp[nums[i]] > n/3){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+//     return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int> ans;
         int n = nums.size();
-        for(int i=0; i<n; i++){
+        vector<int> ls;
+        int min = n / 3 + 1;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
             mp[nums[i]]++;
-            if(find(ans.begin(), ans.end(), nums[i]) == ans.end() && mp[nums[i]] > n/3){
-                ans.push_back(nums[i]);
+            if (mp[nums[i]] == min) {
+                ls.push_back(nums[i]);
             }
         }
-    return ans;
+        return ls;
     }
 };
