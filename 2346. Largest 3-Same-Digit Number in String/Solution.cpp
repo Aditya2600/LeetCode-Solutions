fd56d24a65024097af0ba3f8a6b8e80fa3426426
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-14 10:53:09 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2346
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 8.888 MB
          Memory percentile: 96.52

--- 2346. Largest 3-Same-Digit Number in String/Solution.cpp
+++ 2346. Largest 3-Same-Digit Number in String/Solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        string ans = "";
+        char ans = -1;
         int n = num.size();
-        for (int i = 0; i + 2 < n; i++) {
-            if (num[i] == num[i+1] && num[i] == num[i+2]) {
-                string cur = num.substr(i, 3);
-                if (cur > ans) ans = cur; // lexicographic comparison works for same length
+        for(int i=2; i<n; i++){
+            if(num[i] == num[i-1] && num[i] == num[i-2]){
+                ans = max(ans, num[i]);
             }
         }
-        return ans;
+        return ans == -1 ? "" : string(3, ans);
     }
 };
