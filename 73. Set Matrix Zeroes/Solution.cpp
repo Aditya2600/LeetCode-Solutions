8898adf9862058926a31a9dcb4cbe2b587516686
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-14 15:50:39 +0530

    C++ Solution
          Submission Statistics:
          Question #: 73
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 18.58 MB
          Memory percentile: 78.78

--- 73. Set Matrix Zeroes/Solution.cpp
+++ 73. Set Matrix Zeroes/Solution.cpp
@@ -1,24 +1,47 @@
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         queue<pair<int,int>> q;
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(matrix[i][j] == 0){
+//                     q.push({i, j});
+//                 }
+//             }
+//         }
+//         while(!q.empty()){
+//             auto [r, c] = q.front();
+//             q.pop();
+//             for(int col = 0; col<m; col++){
+//                 matrix[r][col] = 0;
+//             }
+//             for(int row = 0; row<n; row++){
+//                 matrix[row][c] = 0;
+//             }
+//         }
+//     }
+// };
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        queue<pair<int,int>> q;
         int n = matrix.size();
         int m = matrix[0].size();
+        vector<int> row(n, 0),col(m, 0);
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    q.push({i, j});
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-        while(!q.empty()){
-            auto [r, c] = q.front();
-            q.pop();
-            for(int col = 0; col<m; col++){
-                matrix[r][col] = 0;
-            }
-            for(int row = 0; row<n; row++){
-                matrix[row][c] = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
