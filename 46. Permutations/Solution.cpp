96362088fa2b3418c68f946a953fa689fb0a18b7
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-15 10:17:46 +0530

    C++ Solution
          Submission Statistics:
          Question #: 46
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 10.544 MB
          Memory percentile: 68.11

--- 46. Permutations/Solution.cpp
+++ 46. Permutations/Solution.cpp
@@ -1,29 +1,51 @@
+// class Solution {
+// public:
+
+//     void recurpermute(vector<int>& nums, vector<int>& ds, vector<bool>& flag, vector<vector<int>>& ans){
+//         int n = nums.size();
+//         if(ds.size() == n){
+//             ans.push_back(ds);
+//             return;
+//         }
+//         for(int i=0; i<n; i++){
+//             if(!flag[i]){
+//                 flag[i] = true;
+//                 ds.push_back(nums[i]);
+//                 recurpermute(nums, ds, flag, ans);
+//                 ds.pop_back();
+//                 flag[i] = false;
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> ans;
+//         vector<int> ds;
+//         vector<bool> flag(n, false);
+//         recurpermute(nums, ds, flag, ans);
+//         return ans;
+//     }
+// };
 class Solution {
 public:
 
-    void recurpermute(vector<int>& nums, vector<int>& ds, vector<bool>& flag, vector<vector<int>>& ans){
+     void recurpermute(int ind, vector<vector<int>>& ans, vector<int>& nums) {
         int n = nums.size();
-        if(ds.size() == n){
-            ans.push_back(ds);
+        if (ind == n) {
+            ans.push_back(nums);
             return;
         }
-        for(int i=0; i<n; i++){
-            if(!flag[i]){
-                flag[i] = true;
-                ds.push_back(nums[i]);
-                recurpermute(nums, ds, flag, ans);
-                ds.pop_back();
-                flag[i] = false;
-            }
+        for (int i = ind; i < n; i++) {
+            swap(nums[ind], nums[i]);
+            recurpermute(ind + 1, ans, nums);
+            swap(nums[i], nums[ind]);
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
         vector<vector<int>> ans;
-        vector<int> ds;
-        vector<bool> flag(n, false);
-        recurpermute(nums, ds, flag, ans);
+        recurpermute(0, ans, nums);
         return ans;
     }
 };
