6a2a4584a185e869414f26a3fb12f8e8866f0541
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-22 17:51:27 +0530

    C++ Solution
          Submission Statistics:
          Question #: 53
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 71.852 MB
          Memory percentile: 16.59

--- 53. Maximum Subarray/Solution.cpp
+++ 53. Maximum Subarray/Solution.cpp
@@ -1,27 +1,17 @@
-#include <vector>
-#include <limits.h> // For INT_MIN
-using namespace std;
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        long long sum = 0; // Use long long to avoid overflow
-        int maxi = INT_MIN; // Initialize to the smallest integer
-
-        for (int i = 0; i < nums.size(); i++) {
+        int maxi = INT_MIN;
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++){
             sum += nums[i];
-
-            // Update maximum sum found so far
-            if (sum > maxi) {
+            if(sum > maxi){
                 maxi = sum;
             }
-
-            // Reset sum if it drops below zero
-            if (sum < 0) {
+            if(sum < 0){
                 sum = 0;
             }
         }
-
         return maxi;
     }
 };
