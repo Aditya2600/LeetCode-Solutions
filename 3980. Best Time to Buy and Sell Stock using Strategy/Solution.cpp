6a83ccc3445759739b928f2a2261884c7c4bc40d
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-22 21:17:23 +0530

    C++ Solution
          Submission Statistics:
          Question #: 3980
          Language: C++
          Runtime: 16 ms
          Runtime percentile: 56.25
          Memory: 302.856 MB
          Memory percentile: 71.78

---
+++ 3980. Best Time to Buy and Sell Stock using Strategy/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        int n = prices.size();
+        long long base = 0;
+        for(int i=0; i<n; i++){
+            base += (prices[i] * strategy[i]);
+        }
+        long long slide = 0;
+        long long ans = base;
+        for(int i=0; i<k; i++) base -= (prices[i] * strategy[i]);
+        for(int i=k/2; i<k; i++) slide += (prices[i]);
+        ans = max(ans, base + slide);
+
+        for(int i=k; i<n; i++){
+            base -= (prices[i] * strategy[i]);
+            base += (prices[i-k] * strategy[i-k]);
+            slide += (prices[i]);
+            slide -= (prices[i-k/2]);
+            ans = max(ans, base + slide);
+        } 
+        return ans;
+    }
+};
