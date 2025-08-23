07b5986bf4344dc13cb6d87d70834baaa212a5b6
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-23 14:04:03 +0530

    C++ Solution
          Submission Statistics:
          Question #: 167
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 19.568 MB
          Memory percentile: 31.82

--- 167. Two Sum II - Input Array Is Sorted/Solution.cpp
+++ 167. Two Sum II - Input Array Is Sorted/Solution.cpp
@@ -1,17 +1,41 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         vector<int> ans;
+//         int n = numbers.size();
+//         unordered_map<int, int> mp;
+//         for(int i=0; i<n; i++){
+//             int req = target - numbers[i];
+//             if(mp.find(req) != mp.end()){
+//                ans.push_back(mp[req]+1);
+//                ans.push_back(i+1);
+//             }
+//              mp[numbers[i]] = i;
+//         }    
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         vector<int> ans;
         int n = numbers.size();
-        unordered_map<int, int> mp;
-        for(int i=0; i<n; i++){
-            int req = target - numbers[i];
-            if(mp.find(req) != mp.end()){
-               ans.push_back(mp[req]+1);
-               ans.push_back(i+1);
+        int i = 0;
+        int j = n-1;
+        while(i<j){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                break;
             }
-             mp[numbers[i]] = i;
-        }    
+            else if(sum > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
         return ans;
     }
 };
