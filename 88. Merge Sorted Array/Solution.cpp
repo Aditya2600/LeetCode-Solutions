2fb83f32d212ff61bac18ebcd6b419a4e608f0f3
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-08-23 11:22:17 +0530

    C++ Solution
          Submission Statistics:
          Question #: 88
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 12.26 MB
          Memory percentile: 70.26

--- 88. Merge Sorted Array/Solution.cpp
+++ 88. Merge Sorted Array/Solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;           // FIX: last valid in nums1
-        int j = n - 1;           // FIX: last in nums2
-        int k = m + n - 1;       // FIX: fill position from the end
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        // Place the larger of nums1[i] and nums2[j] at nums1[k], move backwards
-        while (j >= 0) {         // FIX: only need to ensure nums2 left gets placed
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];   // FIX: was nums2[i--]
             } else {
                 nums1[k--] = nums2[j--];
             }
         }
+        while (j >= 0) nums1[k--] = nums2[j--];  // FIX: copy remaining nums2
+        // (No need to copy remaining nums1; they are already in place.)
     }
 };
